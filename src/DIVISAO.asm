7bbfd30a9f1ef1f203332deb0d4b3b5d1bc07594
André <andredornelas23@gmail.com>
2023-07-16 15:40:15 -0300

    implementa divisão de 16 bits

--- src/DIVISAO.asm
+++ src/DIVISAO.asm
@@ -1,29 +1,53 @@
 global DIVISAO 
 
 extern read_int
+extern read_int_16
 extern int_to_string
 
-%define arg1 dword[EBP-4] 
-%define arg2 dword[EBP-8] 
+extern precision
+
+%define arg1 dword[EBP-4]
+%define arg2 dword[EBP-8]
+
+%define arg1_16 word[EBP-4]
+%define arg2_16 word[EBP-6]
 
 section .text
     DIVISAO:
         enter 8,0
         push ebx
 
-        call read_int
-        mov arg1, eax
+        cmp byte [precision], 0x30 ; se for '0' é 16 bits
+        jne prec32
+            call read_int_16
+            mov arg1_16, ax
 
-        call read_int
-        mov arg2, eax
+            call read_int_16
+            mov arg2_16, ax
 
-        mov eax, arg1
-        mov ebx, arg2
-        cdq 
-        idiv ebx 
+            mov ax, arg1_16
+            mov bx, arg2_16
+            xor edx, edx
+            cwd
+            idiv bx
 
-        call int_to_string
+            jmp final_DIVISAO
 
-        pop ebx
+
+        prec32:
+            call read_int
+            mov arg1, eax
+
+            call read_int
+            mov arg2, eax
+
+            mov eax, arg1
+            mov ebx, arg2
+            cdq 
+            idiv ebx 
+
+        final_DIVISAO:
+            call int_to_string
+            pop ebx
         leave
         ret
